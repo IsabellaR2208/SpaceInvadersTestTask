a014abb76f21320f696371181843b19d47a492b4
IsabellaR2208 <bella.r22@gmail.com>
2024-06-26 22:08:28 +0300

    Clean UIScript

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -8,22 +8,24 @@ public class UIScript : MonoBehaviour {
 
 	private Text startLabel;
 
-	void Start () {
-		startLabel = GetComponent<Text> ();
+	void Start()
+	{
+		startLabel = GetComponent<Text>();
 
-		if (Application.platform == RuntimePlatform.Android)
-			startLabel.text = "Tap to Continue";
-		else
-			startLabel.text = "Press Start to Continue";
+#if UNITY_IOS || UNITY_ANDROID
+		startLabel.text = "Tap to Continue";
+#else
+		startLabel.text = "Press Start to Continue";
+#endif
 	}
 
 	void Update () {
-		if (Application.platform == RuntimePlatform.Android) {
+#if UNITY_IOS || UNITY_ANDROID
 			if (Input.touchCount > 0)
 				SceneManager.LoadScene ("main");
-		} else {
-			if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space))
+#else
+		if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space))
 				SceneManager.LoadScene ("main");
-		}
+#endif
 	}
 }
