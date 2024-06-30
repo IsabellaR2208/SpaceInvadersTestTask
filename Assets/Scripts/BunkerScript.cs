4ac81ac28cd71ff140b7f15d0fa53d6cdde8e3a7
IsabellaR2208 <bella.r22@gmail.com>
2024-06-30 12:34:23 +0300

    Refactor bunker script
    Refactor bunker script

--- Assets/Scripts/BunkerScript.cs
+++ Assets/Scripts/BunkerScript.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BunkerScript : MonoBehaviour {
-
-	void OnTriggerEnter2D (Collider2D col) {
-		if (col.gameObject.tag == "EnemyBullet" || col.gameObject.tag == "PlayerBullet") {
+	const string ENEMY_BULLET = "EnemyBullet";
+	const string PLAYER_BULLET = "PlayerBullet";
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.tag == ENEMY_BULLET || col.gameObject.tag == PLAYER_BULLET) {
 			Destroy (gameObject);
 			Destroy (col.gameObject);
 		}
